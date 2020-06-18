55da2fbbc184a9d441d4acb79102b51f52efc7b9
Muhammad Gufron <mgufron223@gmail.com>
2020-06-18 08:12:20 +0700

    Update sw.js

--- hello/sw.js
+++ hello/sw.js
@@ -1,9 +1,9 @@
 var cacheName = 'hello-pwa';
 var filesToCache = [
-  '/',
-  '/index.html',
-  '/css/style.css',
-  '/js/main.js'
+  '/profile/hello/',
+  '/profile/hello/index.html',
+  '/profile/hello/css/style.css',
+  '/profile/hello/js/main.js'
 ];
 
 /* Start the service worker and cache all of the app's content */
